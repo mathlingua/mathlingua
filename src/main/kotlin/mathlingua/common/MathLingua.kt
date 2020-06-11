da17b30d13c3e4b0bdbe4a1409f31f44a111ad67
Dominic Kramer <DominicKramer@gmail.com>
2020-06-10 23:58:05 -0700

    feat: add Mathlingua.printExpanded() function (#144)

--- src/main/kotlin/mathlingua/common/MathLingua.kt
+++ src/main/kotlin/mathlingua/common/MathLingua.kt
@@ -189,4 +189,162 @@ object MathLingua {
             }
         }
     }
+
+    fun printExpanded(input: String, html: Boolean): Validation<String> {
+        return when (val validation = parse(input)) {
+            is ValidationFailure -> validationFailure(validation.errors)
+            is ValidationSuccess -> {
+                val doc = validation.value
+                val defines = doc.defines
+                validationSuccess(prettyPrint(doc, defines, html))
+            }
+        }
+    }
+
+    fun prettyPrint(node: Phase2Node, defines: List<DefinesGroup>, html: Boolean): String {
+        val writer = if (html) {
+            HtmlCodeWriter(defines = defines)
+        } else {
+            MathLinguaCodeWriter(defines = defines)
+        }
+        val code = node.toCode(false, 0, writer = writer).getCode()
+        return getHtml(code.replace("<br/><br/><br/>", "<br/><br/>"))
+    }
 }
+
+private fun getHtml(body: String) = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css"
+              integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq"
+              crossorigin="anonymous">
+        <script defer
+                src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js"
+                integrity="sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz"
+                crossorigin="anonymous">
+        </script>
+        <script>
+            function buildMathFragment(text) {
+                const fragment = document.createDocumentFragment();
+                var buffer = '';
+                var i = 0;
+                while (i < text.length) {
+                    if (text[i] === '\\' && text[i+1] === '[') {
+                        i += 2; // skip over \ and [
+                        fragment.appendChild(document.createTextNode(buffer));
+                        buffer = '';
+
+                        const span = document.createElement('span');
+                        var math = '';
+                        while (i < text.length &&
+                            !(text[i] === '\\' && text[i+1] === ']')) {
+                            math += text[i++];
+                        }
+                        if (text[i] === '\\') {
+                            i++; // move past the \
+                        }
+                        if (text[i] === ']') {
+                            i++; // move past the ]
+                        }
+                        try {
+                            katex.render(math, span, {
+                                throwOnError: true,
+                                displayMode: true
+                            });
+                        } catch {
+                            span.appendChild(document.createTextNode(math));
+                        }
+                        fragment.appendChild(span);
+                    } else {
+                        buffer += text[i++];
+                    }
+                }
+
+                if (buffer.length > 0) {
+                    fragment.appendChild(document.createTextNode(buffer));
+                }
+
+                return fragment;
+            }
+
+            function render(node) {
+                for (let i = 0; i < node.childNodes.length; i++) {
+                    const child = node.childNodes[i];
+
+                    // node is an element node => nodeType === 1
+                    // node is an attribute node => nodeType === 2
+                    // node is a text node => nodeType === 3
+                    // node is a comment node => nodeType === 8
+                    if (child.nodeType === 3) {
+                        const text = child.textContent;
+                        if (text.trim()) {
+                            const fragment = buildMathFragment(text);
+                            i += fragment.childNodes.length - 1;
+                            node.replaceChild(fragment, child);
+                        }
+                    } else if (child.nodeType === 1) {
+                        render(child);
+                    }
+                }
+            }
+        </script>
+        <style>
+            .mathlingua {
+                font-family: monospace;
+            }
+
+            .mathlingua-header {
+                font-weight: bold;
+                color: #0055bb;
+            }
+
+            .mathlingua-whitespace {
+                padding: 0;
+                margin: 0;
+                margin-left: 1ex;
+            }
+
+            .mathlingua-id {
+                margin-left: -0.2em;
+                font-weight: bold;
+                color: #5500aa;
+            }
+
+            .mathlingua-text {
+                color: #007700;
+            }
+
+            .mathlingua-top-level {
+                box-shadow: 0px 0px 2px 0px #888888;
+                padding: 1em;
+                display: block;
+                width: fit-content;
+                overflow: scroll;
+                margin-left: auto;
+                margin-right: auto;
+            }
+
+            .katex {
+                font-size: 0.75em;
+            }
+
+            .katex-display {
+                display: contents;
+            }
+
+            .katex-display > .katex {
+                display: contents;
+            }
+
+            .katex-display > .katex > .katex-html {
+                display: contents;
+            }
+        </style>
+    </head>
+    <body onload="render(document.body)">
+        $body
+    </body>
+</html>
+"""

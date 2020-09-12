30568769145ce5585f61a215205165517dc03d73
Dominic Kramer <DominicKramer@gmail.com>
2020-09-11 23:27:58 -0700

    feat: support `collection:of:over:for:where:`
    These groups are used to describe sets.

---
+++ src/main/kotlin/mathlingua/common/chalktalk/phase2/ast/section/OverSection.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mathlingua.common.chalktalk.phase2.ast.section
+
+import mathlingua.common.MutableLocationTracker
+import mathlingua.common.chalktalk.phase1.ast.Phase1Node
+import mathlingua.common.chalktalk.phase2.CodeWriter
+import mathlingua.common.chalktalk.phase2.ast.Phase2Node
+import mathlingua.common.chalktalk.phase2.ast.clause.Statement
+
+data class OverSection(val statement: Statement) : Phase2Node {
+    override fun forEach(fn: (node: Phase2Node) -> Unit) {
+        fn(statement)
+    }
+
+    override fun toCode(isArg: Boolean, indent: Int, writer: CodeWriter): CodeWriter {
+        writer.writeIndent(isArg, indent)
+        writer.writeHeader("over")
+        writer.append(statement, false, 1)
+        return writer
+    }
+
+    override fun transform(chalkTransformer: (node: Phase2Node) -> Phase2Node) =
+        chalkTransformer(OverSection(
+            statement = statement.transform(chalkTransformer) as Statement
+        ))
+}
+
+fun validateOverSection(node: Phase1Node, tracker: MutableLocationTracker) = validateStatementSection(
+    node,
+    tracker,
+    "over",
+    ::OverSection
+)

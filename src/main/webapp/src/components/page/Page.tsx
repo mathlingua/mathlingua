dd966fcfa16248a98b05997f7e9d9926e12224e4
Dominic Kramer <DominicKramer@gmail.com>
2021-09-19 22:52:09 -0700

    client: ensure editor is always 100vh high

--- src/main/webapp/src/components/page/Page.tsx
+++ src/main/webapp/src/components/page/Page.tsx
@@ -298,6 +298,7 @@ export const Page = (props: PageProps) => {
       style={{
         width: '100%',
         height: '100%',
+        minHeight: '100vh',
       }}
       annotations={annotations}
     ></AceEditor>

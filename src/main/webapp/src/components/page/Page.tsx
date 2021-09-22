907355b659e526a59f7fddb8f0ab97e90625d259
Dominic Kramer <DominicKramer@gmail.com>
2021-09-21 20:07:58 -0700

    client: the editor always takes all verical space

--- src/main/webapp/src/components/page/Page.tsx
+++ src/main/webapp/src/components/page/Page.tsx
@@ -306,6 +306,7 @@ export const Page = (props: PageProps) => {
         position: 'relative',
         width: '100%',
         height: '100%',
+        minHeight: '100vh',
       }}
       annotations={annotations}
     ></AceEditor>

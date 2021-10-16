12e51a4797c6b70b307284664adb53b2d3b2ebbe
Dominic Kramer <DominicKramer@gmail.com>
2021-10-16 00:22:10 -0700

    chore: move scripts to new `bin` directory

--- bin/cypress.sh
+++ bin/cypress.sh
@@ -6,4 +6,4 @@ set -e
 ./gradlew build
 
 cd src/main/webapp/cypress-codex
-java -jar ../../../../build/releases/mathlingua-0.10.1.jar edit
+java -jar ../../../../build/releases/mathlingua-*.jar edit

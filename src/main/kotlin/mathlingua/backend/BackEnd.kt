a5c529388287f4bccf8df119cb53b45764e018f4
Dominic Kramer <DominicKramer@gmail.com>
2022-01-08 21:01:31 -0800

    chore: simplify `BackEnd`

--- src/main/kotlin/mathlingua/backend/BackEnd.kt
+++ src/main/kotlin/mathlingua/backend/BackEnd.kt
@@ -16,21 +16,20 @@
 
 package mathlingua.backend
 
-import mathlingua.frontend.support.Location
 import mathlingua.frontend.support.ParseError
 
 internal object BackEnd {
     fun check(sourceCollection: SourceCollection): List<ValueSourceTracker<ParseError>> {
         val errors = mutableListOf<ValueSourceTracker<ParseError>>()
-        errors.addAll(checkParseErrors(sourceCollection))
+        errors.addAll(sourceCollection.getParseErrors())
         errors.addAll(checkUndefinedSignatures(sourceCollection))
         errors.addAll(checkDuplicateDefinedSignatures(sourceCollection))
-        errors.addAll(checkInvalidTypes(sourceCollection))
-        errors.addAll(checkSymbolErrors(sourceCollection))
-        errors.addAll(checkIsRhs(sourceCollection))
-        errors.addAll(checkColonEqualsRhs(sourceCollection))
-        errors.addAll(checkInputOutputSymbolErrors(sourceCollection))
-        errors.addAll(checkNonExpressesUsedInNonIsStatements(sourceCollection))
+        errors.addAll(sourceCollection.findInvalidTypes())
+        errors.addAll(sourceCollection.getSymbolErrors())
+        errors.addAll(sourceCollection.getIsRhsErrors())
+        errors.addAll(sourceCollection.getColonEqualsRhsErrors())
+        errors.addAll(sourceCollection.getInputOutputSymbolErrors())
+        errors.addAll(sourceCollection.getNonExpressesUsedInNonIsNonInStatementsErrors())
         return errors
     }
 
@@ -57,59 +56,4 @@ internal object BackEnd {
                         row = it.value.location.row,
                         column = it.value.location.column))
         }
-
-    private fun checkInvalidTypes(sourceCollection: SourceCollection) =
-        sourceCollection.findInvalidTypes()
-
-    private fun checkParseErrors(sourceCollection: SourceCollection) =
-        sourceCollection.getParseErrors()
-
-    private fun checkDuplicateContent(
-        sourceCollection: SourceCollection
-    ): List<ValueSourceTracker<ParseError>> {
-        val errors = mutableListOf<ValueSourceTracker<ParseError>>()
-        for (grp in sourceCollection.getDuplicateContent()) {
-            val location = grp.tracker?.getLocationOf(grp.value) ?: Location(row = -1, column = -1)
-            errors.add(
-                ValueSourceTracker(
-                    source = grp.source,
-                    tracker = grp.tracker,
-                    value =
-                        ParseError(
-                            message = "Duplicate content detected",
-                            row = location.row,
-                            column = location.column)))
-        }
-        return errors
-    }
-
-    private fun checkSymbolErrors(
-        sourceCollection: SourceCollection
-    ): List<ValueSourceTracker<ParseError>> {
-        return sourceCollection.getSymbolErrors()
-    }
-
-    private fun checkIsRhs(
-        sourceCollection: SourceCollection
-    ): List<ValueSourceTracker<ParseError>> {
-        return sourceCollection.getIsRhsErrors()
-    }
-
-    private fun checkColonEqualsRhs(
-        sourceCollection: SourceCollection
-    ): List<ValueSourceTracker<ParseError>> {
-        return sourceCollection.getColonEqualsRhsErrors()
-    }
-
-    private fun checkInputOutputSymbolErrors(
-        sourceCollection: SourceCollection
-    ): List<ValueSourceTracker<ParseError>> {
-        return sourceCollection.getInputOutputSymbolErrors()
-    }
-
-    private fun checkNonExpressesUsedInNonIsStatements(
-        sourceCollection: SourceCollection
-    ): List<ValueSourceTracker<ParseError>> {
-        return sourceCollection.getNonExpressesUsedInNonIsNonInStatementsErrors()
-    }
 }

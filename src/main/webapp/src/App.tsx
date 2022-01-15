d466b3e74035438a317f006e1260670095e02ca6
Dominic Kramer <DominicKramer@gmail.com>
2022-01-14 22:44:09 -0800

    client: add support for a cookies banner
    Support for having a cookies banner exists in the code but is
    not yet enabled since it is not needed unless analytics is used
    which is currently turned off.

--- src/main/webapp/src/App.tsx
+++ src/main/webapp/src/App.tsx
@@ -1,6 +1,6 @@
 import styles from './App.module.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import { ContentPanel } from './components/content-panel/ContentPanel';
@@ -8,10 +8,14 @@ import { ReferencePanel } from './components/reference/ReferencePanel';
 import { TexTalkReferencePanel } from './components/reference/tex-talk-reference-panel/TexTalkReferencePanel';
 import { ChalkTalkReferencePanel } from './components/reference/chalk-talk-reference-panel/ChalkTalkReferencePanel';
 
+import CookieConsent from 'react-cookie-consent';
+
 // import ReactGA from 'react-ga4';
-// ReactGA.initialize('G-JPSYF6C3L8');
+let analyticsInitialized = false;
+const showBanner = false;
 
 export const App = () => {
+  const [allowAnalytics, setAllowAnalytics] = useState(false);
   return (
     <HashRouter hashType="slash">
       <ErrorBoundary>
@@ -36,15 +40,52 @@ export const App = () => {
                     })
                   }
                   onLocationChanged={(path) => {
+                    if (allowAnalytics) {
+                      if (!analyticsInitialized) {
+                        // ReactGA.initialize('G-JPSYF6C3L8');
+                        analyticsInitialized = true;
+                      }
 //                    ReactGA.event('page_view', {
 //                      page_title: path
 //                    });
+                    }
                   }}
                 />
               )}
             />
           </Switch>
         </div>
+        {
+          showBanner ?
+          <CookieConsent
+            buttonText='I agree'
+            buttonStyle={{
+              backgroundColor: '#70c273',
+              color: '#ffffff',
+              borderRadius: '3px'
+            }}
+            declineButtonText='I disagree'
+            declineButtonStyle={{
+              backgroundColor: '#d15858',
+              color: '#ffffff',
+              borderRadius: '3px'
+            }}
+            enableDeclineButton={true}
+            acceptOnScroll={false}
+            style={{
+              fontFamily: 'Georgia, "Times New Roman", Times, serif',
+              color: '#000000',
+              borderTop: 'solid',
+              borderTopColor: '#cccccc',
+              borderTopWidth: '1px',
+              background: '#f5f5f8'
+            }}
+            onAccept={() => setAllowAnalytics(true)}
+            onDecline={() => setAllowAnalytics(false)}
+            debug={true}>
+            If you agree, this site will use cookies to analyze its traffic.
+          </CookieConsent> : null
+        }
       </ErrorBoundary>
     </HashRouter>
   );

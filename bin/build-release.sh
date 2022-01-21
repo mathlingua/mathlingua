e32b10030d7d271547ef3d02407230081b237587
Dominic Kramer <DominicKramer@gmail.com>
2022-01-20 22:58:13 -0800

    feat: `build-release.sh` can notarize MacOS binaries
    If the `--notarize` option is supplied, then the MacOS binaries
    will be signed and notarized.  To do this, the
    `MLG_DEV_APPLE_CERTIFICATE_ID` and `MLG_DEV_APPLE_EMAIL`
    environment variables need to be set.  See the script for more
    details.

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -60,26 +60,73 @@ done
 
 cd ..
 
+echo "Making all of the built binaries executable"
+chmod +x release/*
+
 ./bin/build-assets.sh
 
-echo "Updating the documentation's mlg"
-mkdir -p documentation
-cp release/mlg-${MLG_VERSION}-darwin-arm64 documentation/mlg
-
+# sign the MacOS binaries if possible and notarize them if
+# the --notarize option is specified
 if command -v codesign &> /dev/null
 then
-  echo "Signing the binary documentation/mlg"
-  codesign -s - documentation/mlg
+  if [ -z "$MLG_DEV_APPLE_CERTIFICATE_ID" ]; then
+    echo "You must set the MLG_DEV_APPLE_CERTIFICATE_ID environment variable"
+    echo "to be Apple Developer Certificate ID to use for signing and notarizing."
+    echo "See https://artyom.dev/notarizing-go-binaries-for-macos.html for details"
+    echo "on how to obtain this ID."
+    exit 1
+  fi
 
-  for file in $(ls release/mlg-*-darwin-*)
-  do
-    echo "Signing the binary ${file}"
-    codesign -s - ${file}
-  done
+  if [ -z "$MLG_DEV_APPLE_EMAIL" ]; then
+    echo "You must set the MLG_DEV_APPLE_EMAIL environment variable to be the"
+    echo "email of the Apple Developer account to use for signing and notarizing."
+    echo "See https://artyom.dev/notarizing-go-binaries-for-macos.html for details"
+    echo "about the notarization process."
+    exit 1
+  fi
+
+  if [ "$1" = "--notarize" ]; then
+    echo "Attempting to notarize the MacOS binaries"
+    for file in $(ls release/mlg-*-darwin-*)
+    do
+      echo "Signing the binary ${file} with the certificate in \$MLG_DEV_APPLE_CERTIFICATE_ID"
+      codesign --timestamp -s "$MLG_DEV_APPLE_CERTIFICATE_ID" -o runtime -v "${file}"
+    done
+
+    MAC_OS_BINARIES_ZIP=release/macos-binaries.zip
+    echo "Bundling the MacOS binaries into ${MAC_OS_BINARIES_ZIP}"
+    zip -j "${MAC_OS_BINARIES_ZIP}" $(ls release/mlg-*-darwin-*)
+
+    echo "Sending the MacOS binaries for notarization"
+    xcrun altool --notarize-app \
+                 --primary-bundle-id com.github.mathlingua \
+                 --username "${MLG_DEV_APPLE_EMAIL}" \
+                 --password '@keychain:altool' \
+                 --file "${MAC_OS_BINARIES_ZIP}"
+
+    echo "Use the following command to check the status of the notarization"
+    echo "process where REQUEST-UUID can be obtained from the output above."
+    echo "You should also receive an email when the notarization completes"
+    echo "or encounters an error."
+    echo ""
+    echo "xcrun altool --notarization-info <REQUEST-UUID> \\"
+    echo "             -u ${MLG_DEV_APPLE_EMAIL} \\"
+    echo "             -p \"@keychain:altool\""
+  else
+    for file in $(ls release/mlg-*-darwin-*)
+    do
+      echo "Ad-hoc signing the binary ${file}"
+      codesign -s - ${file}
+    done
+  fi
 else
   echo "WARNING: Could not sign the binaries since 'codesign' does not exist."
   echo "         Build the binaries on a MacOS system to be able to sign the binaries."
 fi
+
+echo "Updating the documentation's mlg"
+mkdir -p documentation
+cp release/mlg-${MLG_VERSION}-darwin-arm64 documentation/mlg
 
 echo "Building the documentation"
 ./bin/build-docs.sh

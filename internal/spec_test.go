595a9e91aff6b3e315a87c2cf6958c366bd1953f
Dominic Kramer <DominicKramer@gmail.com>
2022-10-13 00:08:18 -0700

    fix the spec test

--- internal/spec_test.go
+++ internal/spec_test.go
@@ -24,258 +24,383 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var all_groups = [][]string{
-	ast.AllOfSections,
-	ast.NotSections,
-	ast.AnyOfSections,
-	ast.OneOfSections,
-	ast.ExistsSections,
-	ast.ExistsUniqueSections,
-	ast.ForAllSections,
-	ast.IfSections,
-	ast.IffSections,
-	ast.GeneratedSections,
-	ast.PiecewiseSections,
-	ast.AsViaSections,
-	ast.AsThroughStatesSections,
-	ast.OperationsSections,
-	ast.MembersSections,
-	ast.ExpressedSections,
-	ast.CalledSections,
-	ast.ExpressingSections,
-	ast.LooselySections,
-	ast.OverviewSections,
-	ast.MotivationSections,
-	ast.HistorySections,
-	ast.ExamplesSections,
-	ast.RelatedSections,
-	ast.DiscoveredSections,
-	ast.NotesSections,
-	ast.LabelSections,
-	ast.BySections,
-	ast.IdSections,
-	ast.DescribesSections,
-	ast.DeclaresSections,
-	ast.StatesSections,
-	ast.ProofSections,
-	ast.AxiomSections,
-	ast.ConjectureSections,
-	ast.TheoremSections,
-	ast.TopicSections,
-	ast.NoteSections,
-	ast.ZeroSections,
-	ast.PositiveIntSections,
-	ast.NegativeIntSections,
-	ast.PositiveFloatSections,
-	ast.NegativeFloatSections,
-	ast.SpecifySections,
+type testCase struct {
+	actual   []string
+	expected []string
+}
+
+var all_group_test_cases = []testCase{
+	{
+		actual: ast.AllOfSections,
+		expected: []string{
+			"allOf",
+		},
+	},
+	{
+		actual: ast.NotSections,
+		expected: []string{
+			"not",
+		},
+	},
+	{
+		actual: ast.AnyOfSections,
+		expected: []string{
+			"anyOf",
+		},
+	},
+	{
+		actual: ast.OneOfSections,
+		expected: []string{
+			"oneOf",
+		},
+	},
+	{
+		actual: ast.ExistsSections,
+		expected: []string{
+			"exists",
+			"where?",
+			"suchThat",
+		},
+	},
+	{
+		actual: ast.ExistsUniqueSections,
+		expected: []string{
+			"existsUnique",
+			"where?",
+			"suchThat?",
+		},
+	},
+	{
+		actual: ast.ForAllSections,
+		expected: []string{
+			"forAll",
+			"where?",
+			"suchThat?",
+			"then",
+		},
+	},
+	{
+		actual: ast.IfSections,
+		expected: []string{
+			"if",
+			"then",
+		},
+	},
+	{
+		actual: ast.IffSections,
+		expected: []string{
+			"iff",
+			"then",
+		},
+	},
+	{
+		actual: ast.PiecewiseSections,
+		expected: []string{
+			"piecewise",
+			"if",
+			"then",
+			"else?",
+		},
+	},
+	{
+		actual: ast.AsViaSections,
+		expected: []string{
+			"as",
+			"via",
+		},
+	},
+	{
+		actual: ast.AsThroughStatesSections,
+		expected: []string{
+			"as",
+			"through",
+			"as?",
+			"states?",
+		},
+	},
+	{
+		actual: ast.OperationsSections,
+		expected: []string{
+			"operations",
+			"given?",
+			"when?",
+			"specify",
+		},
+	},
+	{
+		actual: ast.MembersSections,
+		expected: []string{
+			"members",
+		},
+	},
+	{
+		actual: ast.ExpressedSections,
+		expected: []string{
+			"expressed",
+		},
+	},
+	{
+		actual: ast.CalledSections,
+		expected: []string{
+			"called",
+		},
+	},
+	{
+		actual: ast.ExpressingSections,
+		expected: []string{
+			"expressing",
+			"as",
+		},
+	},
+	{
+		actual: ast.LooselySections,
+		expected: []string{
+			"loosely",
+		},
+	},
+	{
+		actual: ast.OverviewSections,
+		expected: []string{
+			"overview",
+		},
+	},
+	{
+		actual: ast.MotivationSections,
+		expected: []string{
+			"motivation",
+		},
+	},
+	{
+		actual: ast.HistorySections,
+		expected: []string{
+			"history",
+		},
+	},
+	{
+		actual: ast.ExamplesSections,
+		expected: []string{
+			"examples",
+		},
+	},
+	{
+		actual: ast.RelatedSections,
+		expected: []string{
+			"related",
+		},
+	},
+	{
+		actual: ast.DiscoveredSections,
+		expected: []string{
+			"discovered",
+		},
+	},
+	{
+		actual: ast.NotesSections,
+		expected: []string{
+			"notes",
+		},
+	},
+	{
+		actual: ast.LabelSections,
+		expected: []string{
+			"label",
+			"by",
+		},
+	},
+	{
+		actual: ast.BySections,
+		expected: []string{
+			"by",
+		},
+	},
+	{
+		actual: ast.IdSections,
+		expected: []string{
+			"id",
+		},
+	},
+	{
+		actual: ast.DescribesSections,
+		expected: []string{
+			"Describes",
+			"with?",
+			"given?",
+			"when?",
+			"suchThat?",
+			"extends?",
+			"satisfies?",
+			"Provides?",
+			"Viewable?",
+			"Justified?",
+			"Documented?",
+			"References?",
+			"Using?",
+			"Metadata?",
+		},
+	},
+	{
+		actual: ast.DeclaresSections,
+		expected: []string{
+			"Declares",
+			"with?",
+			"given?",
+			"when?",
+			"suchThat?",
+			"means?",
+			"defines?",
+			"Provides?",
+			"Viewable?",
+			"Justified?",
+			"Documented?",
+			"References?",
+			"Using?",
+			"Metadata?",
+		},
+	},
+	{
+		actual: ast.StatesSections,
+		expected: []string{
+			"states",
+			"given?",
+			"when?",
+			"suchThat?",
+			"that",
+			"Documented?",
+			"Justified?",
+			"References?",
+			"Using?",
+			"Metadata?",
+		},
+	},
+	{
+		actual: ast.ProofSections,
+		expected: []string{
+			"Proof",
+			"of",
+			"content",
+			"References?",
+			"Metadata?",
+		},
+	},
+	{
+		actual: ast.AxiomSections,
+		expected: []string{
+			"Axiom",
+			"given?",
+			"where?",
+			"suchThat?",
+			"then",
+			"iff?",
+			"Documented?",
+			"References?",
+			"Using?",
+			"Metadata?",
+		},
+	},
+	{
+		actual: ast.ConjectureSections,
+		expected: []string{
+			"Conjecture",
+			"given?",
+			"where?",
+			"suchThat?",
+			"then",
+			"iff?",
+			"Documented?",
+			"References?",
+			"Using?",
+			"Metadata?",
+		},
+	},
+	{
+		actual: ast.TheoremSections,
+		expected: []string{
+			"Theorem",
+			"given?",
+			"where?",
+			"suchThat?",
+			"then",
+			"iff?",
+			"Proof?",
+			"Documented?",
+			"References?",
+			"Using?",
+			"Metadata?",
+		},
+	},
+	{
+		actual: ast.TopicSections,
+		expected: []string{
+			"Topic",
+			"content",
+			"References?",
+			"Metadata?",
+		},
+	},
+	{
+		actual: ast.NoteSections,
+		expected: []string{
+			"Note",
+			"content",
+			"Metadata?",
+		},
+	},
+	{
+		actual: ast.PositiveIntSections,
+		expected: []string{
+			"positiveInt",
+			"is",
+		},
+	},
+	{
+		actual: ast.NegativeIntSections,
+		expected: []string{
+			"negativeInt",
+			"is",
+		},
+	},
+	{
+		actual: ast.ZeroSections,
+		expected: []string{
+			"zero",
+			"is",
+		},
+	},
+	{
+		actual: ast.PositiveFloatSections,
+		expected: []string{
+			"positiveFloat",
+			"is",
+		},
+	},
+	{
+		actual: ast.NegativeFloatSections,
+		expected: []string{
+			"negativeFloat",
+			"is",
+		},
+	},
+	{
+		actual: ast.SpecifySections,
+		expected: []string{
+			"Specify",
+		},
+	},
 }
 
 func TestSpec(t *testing.T) {
-	t.Skip("Skipping since the code is not aligned with the spec")
+	for _, testCase := range all_group_test_cases {
+		actual := "\n"
+		expected := "\n"
 
-	actual := "\n"
-	for _, sections := range all_groups {
-		for _, section := range sections {
+		for _, section := range testCase.actual {
 			actual += fmt.Sprintf("%s:\n", section)
 		}
-		actual += "\n"
+
+		for _, section := range testCase.expected {
+			expected += fmt.Sprintf("%s:\n", section)
+		}
+
+		assert.Equal(t, expected, actual)
 	}
-
-	expected := `
-allOf:
-
-not:
-
-anyOf:
-
-oneOf:
-
-exists:
-where?:
-suchThat:
-
-existsUnique:
-where?:
-suchThat?:
-
-forAll:
-where?:
-suchThat?:
-then:
-
-if:
-then:
-
-iff:
-then:
-
-generated:
-from:
-when?:
-
-piecewise:
-if:
-then:
-else?:
-
-as:
-via:
-
-as:
-through:
-as?:
-states?:
-
-operations:
-on?:
-using?:
-when?:
-specify:
-
-members:
-
-expressed:
-
-called:
-
-expressing:
-as:
-
-loosely:
-
-overview:
-
-motivation:
-
-history:
-
-examples:
-
-related:
-
-discovered:
-
-notes:
-
-label:
-by:
-
-by:
-
-id:
-
-Describes:
-with?:
-using?:
-provided?:
-suchThat?:
-extends?:
-satisfies?:
-Provides?:
-Viewable?:
-Justified?:
-Documented?:
-References?:
-Aliases?:
-Metadata?:
-
-Declares:
-with?:
-using?:
-provided?:
-suchThat?:
-means?:
-defines?:
-Provides?:
-Viewable?:
-Justified?:
-Documented?:
-References?:
-Aliases?:
-Metadata?:
-
-States:
-using?:
-provided?:
-suchThat?:
-that:
-Documented?:
-Justified?:
-References?:
-Aliases?:
-Metadata?:
-
-Proof:
-of:
-content:
-References?:
-Metadata?:
-
-Axiom:
-given?:
-where?:
-suchThat?:
-then:
-iff?:
-Documented?:
-References?:
-Aliases?:
-Metadata?:
-
-Conjecture:
-given?:
-where?:
-suchThat?:
-then:
-iff?:
-Documented?:
-References?:
-Aliases?:
-Metadata?:
-
-Theorem:
-given?:
-where?:
-suchThat?:
-then:
-iff?:
-Proof?:
-Documented?:
-References?:
-Aliases?:
-Metadata?:
-
-Topic:
-content:
-References?:
-Metadata?:
-
-Note:
-content:
-Metadata?:
-
-zero:
-is:
-
-positiveInt:
-is:
-
-negativeInt:
-is:
-
-positiveFloat:
-is:
-
-negativeFloat:
-is:
-
-Specify:
-
-when:
-then:
-
-`
-
-	assert.Equal(t, expected, actual)
 }

f571bb4ecf6c8e9e1323c2ac35cbfff709db8abb
Dominic Kramer <DominicKramer@gmail.com>
2023-03-19 16:33:04 -0700

    initial commit of revamped backend code

---
+++ internal/backend2/pattern.go
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2023 Dominic Kramer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package backend2
+
+// A pattern describes the shape of inputs to a Defines, Describes, States
+// provides, expression alias, or spec alias.
+type IStaticPattern interface {
+	IStaticPattern()
+}
+
+func (NameStaticPattern) IStaticPattern()           {}
+func (FunctionStaticPattern) IStaticPattern()       {}
+func (TupleStaticPattern) IStaticPattern()          {}
+func (SetStaticPattern) IStaticPattern()            {}
+func (InfixStaticPattern) IStaticPattern()          {}
+func (PrefixStaticPattern) IStaticPattern()         {}
+func (PostfixStaticPattern) IStaticPattern()        {}
+func (CommandStaticPattern) IStaticPattern()        {}
+func (MemberNameStaticPattern) IStaticPattern()     {}
+func (MemberFunctionStaticPattern) IStaticPattern() {}
+func (MemberInfixStaticPattern) IStaticPattern()    {}
+func (MemberPrefixStaticPattern) IStaticPattern()   {}
+func (MemberPostfixStaticPattern) IStaticPattern()  {}
+func (SpecStaticPattern) IStaticPattern()           {}
+
+type NameStaticPattern struct {
+	Name string
+}
+
+type FunctionStaticPattern struct {
+	Name   string
+	Inputs []IStaticPattern
+	Output IStaticPattern
+}
+
+type TupleStaticPattern struct {
+	Items []IStaticPattern
+}
+
+type SetStaticPattern struct {
+	Target    IStaticPattern
+	Condition IStaticPattern
+}
+
+type InfixStaticPattern struct {
+	Name string
+	Lhs  IStaticPattern
+	Rhs  IStaticPattern
+}
+
+type PrefixStaticPattern struct {
+	Name string
+	Arg  IStaticPattern
+}
+
+type PostfixStaticPattern struct {
+	Name string
+	Arg  IStaticPattern
+}
+
+type CommandStaticPattern struct {
+	CurlyArgs   []IStaticPattern
+	ParenArgs   []IStaticPattern
+	NamedGroups []NamedGroupStaticPattern
+}
+
+type NamedGroupStaticPattern struct {
+	Name string
+	Args []IStaticPattern
+}
+
+type MemberNameStaticPattern struct {
+	Target string
+	Member NameStaticPattern
+}
+
+type MemberFunctionStaticPattern struct {
+	Target string
+	Member FunctionStaticPattern
+}
+
+type MemberInfixStaticPattern struct {
+	Target string
+	Member InfixStaticPattern
+}
+
+type MemberPrefixStaticPattern struct {
+	Target string
+	Member PrefixStaticPattern
+}
+
+type MemberPostfixStaticPattern struct {
+	Target string
+	Member PostfixStaticPattern
+}
+
+type SpecStaticPattern struct {
+	Lhs  IStaticPattern
+	Name string
+	Rhs  IStaticPattern
+}
---
+++ internal/backend2/summary.go
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2023 Dominic Kramer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package backend2
+
+import "mathlingua/internal/ast"
+
+type DescribesSummary struct {
+	DefScope    IScopeView
+	Input       IStaticPattern
+	Output      IStaticPattern
+	Usings      []IStaticPattern
+	When        []IConstraint
+	Extends     []IConstraint
+	ExpAliases  []IExpAliasSummary
+	SpecAliases []SpecAliasSummary
+	Written     []WrittenSummary
+	Called      []CalledSummary
+}
+
+type DefinesSummary struct {
+	DefScope    IScopeView
+	Input       IStaticPattern
+	Output      IStaticPattern
+	Usings      []IStaticPattern
+	When        []IConstraint
+	Means       []IConstraint
+	ExpAliases  []IExpAliasSummary
+	SpecAliases []SpecAliasSummary
+	Written     []WrittenSummary
+	Called      []CalledSummary
+}
+
+type StatesSummary struct {
+	DefScope    IScopeView
+	Input       IStaticPattern
+	Output      IStaticPattern
+	Usings      []IStaticPattern
+	ExpAliases  []IExpAliasSummary
+	SpecAliases []SpecAliasSummary
+	Written     []WrittenSummary
+	Called      []CalledSummary
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type ISpecAliasSummaryRhs interface {
+	ISpecAliasSummaryRhs()
+}
+
+func (IsConstraint) ISpecAliasSummaryRhs()   {}
+func (SpecConstraint) ISpecAliasSummaryRhs() {}
+
+type SpecAliasSummary struct {
+	Lhs SpecStaticPattern
+	Rhs ISpecAliasSummaryRhs
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type IExpAliasSummary interface {
+	IExpAliasSummary()
+}
+
+func (InfixExpAliasSummary) IExpAliasSummary()          {}
+func (PrefixExpAliasSummary) IExpAliasSummary()         {}
+func (PostfixExpAliasSummary) IExpAliasSummary()        {}
+func (FunctionExpAliasSummary) IExpAliasSummary()       {}
+func (CommandExpAliasSummary) IExpAliasSummary()        {}
+func (MemberNameExpAliasSummary) IExpAliasSummary()     {}
+func (MemberFunctionExpAliasSummary) IExpAliasSummary() {}
+func (MemberInfixExpAliasSummary) IExpAliasSummary()    {}
+func (MemberPrefixExpAliasSummary) IExpAliasSummary()   {}
+func (MemberPostfixExpAliasSummary) IExpAliasSummary()  {}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type InfixExpAliasSummary struct {
+	Lhs InfixStaticPattern
+	Rhs ast.ExpressionType
+}
+
+type PrefixExpAliasSummary struct {
+	Lhs PrefixStaticPattern
+	Rhs ast.ExpressionType
+}
+
+type PostfixExpAliasSummary struct {
+	Lhs PostfixStaticPattern
+	Rhs ast.ExpressionType
+}
+
+type FunctionExpAliasSummary struct {
+	Lhs FunctionStaticPattern
+	Rhs ast.ExpressionType
+}
+
+type CommandExpAliasSummary struct {
+	Lsh CommandStaticPattern
+	Rhs ast.ExpressionType
+}
+
+type MemberNameExpAliasSummary struct {
+	Lsh MemberNameStaticPattern
+	Rhs ast.ExpressionType
+}
+
+type MemberFunctionExpAliasSummary struct {
+	Lsh MemberFunctionStaticPattern
+	Rhs ast.ExpressionType
+}
+
+type MemberInfixExpAliasSummary struct {
+	Lsh MemberInfixStaticPattern
+	Rhs ast.ExpressionType
+}
+
+type MemberPrefixExpAliasSummary struct {
+	Lhs MemberPrefixStaticPattern
+	Rhs ast.ExpressionType
+}
+
+type MemberPostfixExpAliasSummary struct {
+	Lsh MemberPostfixStaticPattern
+	Rhs ast.ExpressionType
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type CalledSummary struct {
+	From   IStaticPattern
+	Called string
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type WrittenSummary struct {
+	From    IStaticPattern
+	Written string
+}

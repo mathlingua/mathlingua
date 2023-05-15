4ab59e9d3a7f244ac54f8adaa3f0b0b87e7e5837
Dominic Kramer <DominicKramer@gmail.com>
2023-05-14 21:13:15 -0700

    fix rendering text

--- web/src/components/ast/TextArgumentDataView.tsx
+++ web/src/components/ast/TextArgumentDataView.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import styles from './TextArgumentDataView.module.css';
+
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+
+import 'katex/dist/katex.min.css';
+
 import { TextArgumentData } from '../../types';
 import { LatexView } from '../../design/LatexView';
 
@@ -15,9 +24,12 @@ export const TextArgumentDataView = (props: TextArgumentDataViewProps) => {
     return <LatexView latex={fn(props.node.Text)} color='black' />
   }
 
+  // For an unknown reason, if the `'$ $' +` below is not provided, then
+  // the ReactMarkdown is incorrectly rendered as a <code> in the DOM
   return (
-    <span>
-      {fn(props.node.Text)}
-    </span>
+    <ReactMarkdown children={fn('$ $' + props.node.Text)}
+                   remarkPlugins={[remarkGfm, remarkMath]}
+                   rehypePlugins={[rehypeKatex]}
+                   className={styles.markdown} />
   );
 };

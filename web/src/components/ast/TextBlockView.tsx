5b0ef1bd3b2616e1fcc2148a8aeb135e8f96db2b
Dominic Kramer <DominicKramer@gmail.com>
2023-06-04 13:50:06 -0700

    support text block ids in the UI

--- web/src/components/ast/TextBlockView.tsx
+++ web/src/components/ast/TextBlockView.tsx
@@ -10,9 +10,24 @@ export interface TextBlockViewProps {
 }
 
 export const TextBlockView = (props: TextBlockViewProps) => {
+  const rawText = props.node.Text;
+  const lines = rawText.split('\n');
+  /*
+   * If the text is of the form:
+   *   some.text\nmore text...
+   * then the first line of text is an id for the text block that
+   * should not be displayed.
+   *
+   * That is, the text block is of the form:
+   *
+   * ::some.text
+   * more text...
+   * ::
+   */
+  const text = lines.length > 1 ? lines.slice(1).join('\n') : rawText;
   return (
     <div className={styles.mathlinguaTopLevelTextBlock}>
-      <MarkdownView text={props.node.Text} />
+      <MarkdownView text={text} />
     </div>
   );
 };

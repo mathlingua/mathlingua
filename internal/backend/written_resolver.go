f1b35f2b49979559ca489c23676d23e6527f0a79
Dominic Kramer <DominicKramer@gmail.com>
2024-05-06 22:51:43 -0700

    render "satisfies" correctly

--- internal/backend/written_resolver.go
+++ internal/backend/written_resolver.go
@@ -399,6 +399,22 @@ func (w *WrittenResolver) formulationNodeToWritten(path ast.Path, mlgNode ast.Ml
 				result += w.formulationNodeToWritten(path, exp)
 			}
 			return result, true
+		case *ast.SatisfiesExpression:
+			result := ""
+			for i, exp := range n.Lhs {
+				if i > 0 {
+					result += ", "
+				}
+				result += w.formulationNodeToWritten(path, exp)
+			}
+			result += " \\textrm{ satisfies } "
+			for i, exp := range n.Rhs {
+				if i > 0 {
+					result += ", "
+				}
+				result += w.formulationNodeToWritten(path, exp)
+			}
+			return result, true
 		case *ast.ExtendsExpression:
 			result := ""
 			for i, exp := range n.Lhs {
